3b0fe57f3b14d9b7b3cbfd0769a37e9146a28017
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-01-07 16:39:41 +0100

    created constant myApiUrl and function getData

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,11 +1,26 @@
-import { useState } from "react";
-import { allPosts } from "../data/post";
+import { useEffect, useState } from "react";
+// import { allPosts } from "../data/post";
 import CardComponent from "./CardComponent"
 import PostForm from "./PostForm";
 
+import axios from "axios";
+
+const myApiUrl = 'http://localhost:3000/posts';
+
 function MainComponent() {
     // inizializzo lo stato posts con i post esistenti
-    const [posts, setPosts] = useState(allPosts);
+    const [posts, setPosts] = useState([]); //useState(allPosts);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    function getData() {
+        axios.get(myApiUrl).then((res) => {
+            console.log(res.data);
+            setPosts(res.data.data);
+        })
+    }
 
     // funzione che accetta il nuovo post, crea una copia dell'array di post esistenti e aggiunge il nuovo
     function handleNewPost(newPost) {
